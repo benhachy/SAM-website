4ab8092afc50da419e162f93f1e3391de3384fd4
Youssef Benhachem <youssef.benhachem@grenoble-inp.org>
2022-07-22 12:01:40 +0200

    Intégration API

--- myteam/myteam/urls.py
+++ myteam/myteam/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('', include('django.contrib.auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 if settings.DEBUG:
